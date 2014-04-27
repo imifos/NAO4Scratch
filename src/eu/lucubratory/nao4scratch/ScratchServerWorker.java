51326b5194b7c438b3815981fa5242e8a347a562
Tasha CARL <tasha@carl.pro>
2014-04-27 15:57:58 +0200

    .

--- src/eu/lucubratory/nao4scratch/ScratchServerWorker.java
+++ src/eu/lucubratory/nao4scratch/ScratchServerWorker.java
@@ -1,15 +1,15 @@
 /*****************************************************************************
 * NAO4Scratch is free software: you can redistribute it and/or modify 
-* it under the terms of the GNU Lesser General Public License as published by 
+* it under the terms of the GNU General Public License as published by 
 * the Free Software Foundation, either version 3 of the License, or 
 * (at your option) any later version. 
 * 
 * NAO4Scratch is distributed in the hope that it will be useful, 
 * but WITHOUT ANY WARRANTY; without even the implied warranty of 
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-* GNU Lesser General Public License for more details. 
+* GNU General Public License for more details. 
 * 
-* A copy of the GNU Lesser General Public License can be found here:
+* A copy of the GNU General Public License can be found here:
 * http://www.gnu.org/licenses/
 *****************************************************************************/
 
